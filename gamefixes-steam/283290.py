3525b0fb020e2e4f2e3ce8ca5b160c97c8a6704c
Keenan Weaver <keenanweaver@protonmail.com>
2024-03-03 10:00:07 -0600

    gog/zoomplatform: add Nosferatu
    Fixed in Wine 9.0, not necessary anymore

---
+++ gamefixes-steam/283290.py
@@ -0,0 +1,10 @@
+""" Nosferatu: The Wrath of Malachi
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    util.protontricks('mfc42')
+    # Cutscene fixes
+    util.protontricks('quartz') 
